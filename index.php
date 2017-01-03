d8e58d4a84b486046e5749b36528aee13ee556d0
raihan <moshiurraihan95@gmail.com>
2017-01-03 21:30:56 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,102 @@
+<!--
+Project Name: Medical Website.
+Date: 01-01-2017.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Medical</title>
+
+        <!--Bootstarp-->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+        <!--Google fonts-->
+
+
+        <!--    theme styling-->
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <!--Start Top Bar content-->
+        <div class="top_bar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-5">
+                        <div class="contact-info">
+                            <a><i class="fa fa-phone-square"></i> support@gmail.com</a>
+                            <a><i class="fa fa-phone-square"></i> Hotline: +008 01710 000 111</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-7">
+                        <div class="login_signup">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li>
+                                    <a href="login.php" ><i class="fa fa-user"></i> Login</a>
+                                </li>
+                                <li>
+                                    <a href="registration.php"><i class="fa fa-lock"></i> Registration</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--End Top Bar -->
+
+        <!--start header content -->
+        <div class="header">
+            <nav class="header navbar navbar-default">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <div class="logo navbar-brand">
+                            <a href="index.php">
+                                <span>R</span>-Hospital
+                            </a>
+                        </div>
+                    </div>
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li>
+                                <a href="#">Home</a>
+                            </li>
+                            <li>
+                                <a href="#">About<i class="fa fa-caret-down" aria-hidden="true"></i></a>
+                            </li>
+                            <li>
+                                <a href="#">Services<i class="fa fa-caret-down" aria-hidden="true"></i></a>
+                            </li>
+                            <li>
+                                <a href="#">Doctors<i class="fa fa-caret-down" aria-hidden="true"></i></a>
+                            </li>
+                            <li>
+                                <a href="#">Gallery</a>
+                            </li>
+                            <li>
+                                <a href="#">Blog</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <!--end header content -->
+        
+        
+        <script src="js/jquery.min.js"
+        <script src="js/bootstrap.min.js"></script>
+        </body>
+    </html>
